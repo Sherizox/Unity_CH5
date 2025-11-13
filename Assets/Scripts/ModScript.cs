0301f03b3fa5939407dee8ac655bd3227debfa83
Shaheryar <shaheryaryar456@gmail.com>
2025-11-13 18:36:29 +0500

    previous task update

--- Assets/Scripts/ModScript.cs
+++ Assets/Scripts/ModScript.cs
@@ -1,48 +1,110 @@
+using System;
 using UnityEngine;
 
 public class ModScript : MonoBehaviour
 {
-    public float move = 1f;
+    [Header("Movement Settings")]
+    public float moveX = 1f;     
+    public float moveY = 1f;     
+    public float xLimit = 5f;     
+    public float yLimit = 5f;     
+
+    [Header("Scaling Settings")]
+    public float minScale = 1f;
+    public float maxScale = 3f;
+    public float scaleInterval = 0.5f; 
+
+    [Header("Color Settings")]
+    public float colorInterval = 0.5f; 
+
+    [Header("Rotation (not used currently)")]
     public float rotate = 50f;
 
-    public float Direction = 5f;   
-    public float DirectionY = 5f;  
-    public float moveY = 1f;       
+    private bool movingRight = true;
+    private bool movingUp = true;
 
-    bool movingRight = true;
-    bool movingUp = true;
+    public Renderer modCube;
 
-    void Update()
+    // Timers for color and scale changes
+    private float scaleTimer = 0f;
+    private float colorTimer = 0f;
+
+    private void Start()
     {
-   
-        //transform.Rotate(0, 0, rotate * Time.deltaTime);
+        if (modCube == null)
+            modCube = GetComponent<Renderer>();
+    }
 
-      
+    private void Update()
+    {
+        MoveObject();
+
+        // Update scale timer
+        scaleTimer += Time.deltaTime;
+        if (scaleTimer >= scaleInterval)
+        {
+            ChangeScale();
+            scaleTimer = 0f;
+        }
+
+        // Update color timer
+        colorTimer += Time.deltaTime;
+        if (colorTimer >= colorInterval)
+        {
+            ChangeColor();
+            colorTimer = 0f;
+        }
+    }
+
+    // Move the object back and forth within boundaries
+    private void MoveObject()
+    {
+        // Horizontal movement
         if (movingRight)
         {
-            transform.Translate(move * Time.deltaTime, 0, 0);
-            if (transform.position.x > Direction)
+            transform.Translate(moveX * Time.deltaTime, 0, 0);
+            if (transform.position.x > xLimit)
                 movingRight = false;
         }
         else
         {
-            transform.Translate(-move * Time.deltaTime, 0, 0);
-            if (transform.position.x < -Direction)
+            transform.Translate(-moveX * Time.deltaTime, 0, 0);
+            if (transform.position.x < -xLimit)
                 movingRight = true;
         }
 
-       
+        // Vertical movement
         if (movingUp)
         {
             transform.Translate(0, moveY * Time.deltaTime, 0);
-            if (transform.position.y > DirectionY)
+            if (transform.position.y > yLimit)
                 movingUp = false;
         }
         else
         {
             transform.Translate(0, -moveY * Time.deltaTime, 0);
-            if (transform.position.y < -DirectionY)
+            if (transform.position.y < -yLimit)
                 movingUp = true;
         }
+    }
+
+    // Change object color randomly
+    private void ChangeColor()
+    {
+        modCube.material.color = new Color(
+            UnityEngine.Random.Range(0f, 1f),
+            UnityEngine.Random.Range(0f, 1f),
+            UnityEngine.Random.Range(0f, 1f)
+        );
+    }
+
+    // Change object scale randomly
+    private void ChangeScale()
+    {
+        transform.localScale = new Vector3(
+            UnityEngine.Random.Range(minScale, maxScale),
+            UnityEngine.Random.Range(minScale, maxScale),
+            UnityEngine.Random.Range(minScale, maxScale)
+        );
     }
 }
